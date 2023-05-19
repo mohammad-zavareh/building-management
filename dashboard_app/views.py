4d6432a0e15196f29ea01ad316a89e4f03269711
mohammad <mohammadzavareh78@gmail.com>
2023-05-19 22:59:00 +0430

    added mixin for dashboard

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -1,8 +1,10 @@
 from django.views.generic import TemplateView
 
+from buildingManagement.mixins import ManagerRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class ManagerDashboard(TemplateView):
+class ManagerDashboard(LoginRequiredMixin, ManagerRequiredMixin,TemplateView):
     template_name = 'manager_dashboard.html'
 
-class ResidentDashboard(TemplateView):
+class ResidentDashboard(LoginRequiredMixin,TemplateView):
     template_name = 'resident_dashboard.html'
