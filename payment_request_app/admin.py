e5ebe8a348d142e435fa8a31dfe9844c5a85e391
mohammad <mohammadzavareh78@gmail.com>
2023-05-19 22:02:32 +0430

    edit payment_request_app.admin

--- payment_request_app/admin.py
+++ payment_request_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import RequestPayment
+
+
+class RequestPaymentAdmin(admin.ModelAdmin):
+    list_display = ('building', 'sheba_number',
+                    'amount',
+                    'paid',
+                    'pay_time',
+                    'request_time'
+                    )
+
+    list_filter = ('building','amount')
+    ordering = ('request_time',)
+    search_fields = ('sheba_number','building')
+
+
+admin.site.register(RequestPayment, RequestPaymentAdmin)
