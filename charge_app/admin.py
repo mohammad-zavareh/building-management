7b63947154b1d16ce75ae7869995d74eab83ee06
mohammad <mohammadzavareh78@gmail.com>
2023-05-19 22:00:10 +0430

    edit charge_app.admin

--- charge_app/admin.py
+++ charge_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ServiceCharge,ServiceChargeStatus
+
+
+class ServiceChargeAdmin(admin.ModelAdmin):
+    list_display = ('title','building', 'get_categories', 'is_active')
+    list_filter = ('expire_time',)
+    ordering = ('title',)
+    search_fields = ('title', 'description')
+
+
+class ServiceChargeStatusAdmin(admin.ModelAdmin):
+    list_display = ('unit', 'service_charge', 'is_paid', 'payment_type_choice')
+    list_filter = ('unit', 'service_charge', 'is_paid')
+    search_fields = ('unit.name', 'service_charge.title')
+
+admin.site.register(ServiceCharge, ServiceChargeAdmin)
+admin.site.register(ServiceChargeStatus, ServiceChargeStatusAdmin)
