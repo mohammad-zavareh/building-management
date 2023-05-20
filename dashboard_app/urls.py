906a6010882c25d4c0ffd93591901251b0ff9a6e
mohammad <mohammadzavareh78@gmail.com>
2023-05-20 22:40:39 +0430

    Separate address panels

--- dashboard_app/urls.py
+++ dashboard_app/urls.py
@@ -5,6 +5,6 @@ from .views import ManagerDashboard,ResidentDashboard
 app_name = 'dashboard_app'
 
 urlpatterns = [
-    path('/dashboard', ManagerDashboard.as_view(), name='manager_dashboard'),
-    path('/dashboard', ResidentDashboard.as_view(), name='resident_dashboard'),
+    path('manager-panel/dashboard', ManagerDashboard.as_view(), name='manager_dashboard'),
+    path('resident-panel/dashboard', ResidentDashboard.as_view(), name='resident_dashboard'),
 ]
