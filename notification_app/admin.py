9166db6201d6e9a708bca07865a88dd755f1f849
mohammad <mohammadzavareh78@gmail.com>
2023-05-26 17:24:07 +0430

    added hits field in notification model

--- notification_app/admin.py
+++ notification_app/admin.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 
 from .models import Notification
 
-
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'building')
+    list_display = ('title', 'building',)
     list_filter = ('building',)
     ordering = ('title',)
     search_fields = ('title',)
-
 
 admin.site.register(Notification, NotificationAdmin)
--- notification_app/models.py
+++ notification_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-from building_app.models import Building
+from building_app.models import Building,Unit
 
 class Notification(models.Model):
     building = models.ForeignKey(Building, on_delete=models.CASCADE, verbose_name='برای ساختمان')
     title = models.CharField(max_length=50, verbose_name='عنوان')
     description = models.TextField(verbose_name='توضیحات')
+    hits = models.ManyToManyField(Unit,verbose_name='بازدید')
 
     def __str__(self):
         return self.title
