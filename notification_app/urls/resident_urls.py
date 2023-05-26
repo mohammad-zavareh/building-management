fc7684f7e8023b84c149ec04139d8e72ab8df953
mohammad <mohammadzavareh78@gmail.com>
2023-05-26 23:38:20 +0430

    added filter by seen or unseen notifications

--- notification_app/urls/resident_urls.py
+++ notification_app/urls/resident_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from ..views.resident_views import NotificationList
+from ..views.resident_views import NotificationList,NotificationFilter
 
 app_name = 'notification_app_resident'
 
 urlpatterns = [
     path('/notification-list', NotificationList.as_view(), name='notification_list'),
+    path('/notification-list/filter/<str:filter>', NotificationFilter.as_view(), name='notification_filter'),
 ]
--- notification_app/views/resident_views.py
+++ notification_app/views/resident_views.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView, DetailView
-
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from notification_app.models import Notification
+
 
 class NotificationList(LoginRequiredMixin, ListView):
     model = Notification
@@ -14,3 +15,23 @@ class NotificationList(LoginRequiredMixin, ListView):
         qs = super().get_queryset().filter(building=building).order_by('-created')
         return qs
 
+
+class NotificationFilter(LoginRequiredMixin, ListView):
+    model = Notification
+    template_name = 'resident/notification-list.html'
+    paginate_by = 10
+
+    def get_queryset(self, *args, **kwargs):
+        building = self.request.user.unit.building
+        unit = self.request.user.unit
+
+        filter = self.kwargs['filter']
+
+        if filter == 'seen':
+            qs = Notification.objects.filter(Q(building_id=building),Q(hits=unit)) # notification seen by unit
+        elif filter == 'unseen':
+            qs = Notification.objects.filter(Q(building_id=building),~Q(hits=unit)) # notification unseen by unit
+        else:
+            qs = Notification.objects.filter(building_id=building).order_by('-created') # all notification
+
+        return qs
