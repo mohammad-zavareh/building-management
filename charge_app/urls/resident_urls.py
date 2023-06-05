d9b3c363dd1335adfbdc9a5d50eab9a68f161fd4
mohammad <mohammadzavareh78@gmail.com>
2023-06-05 20:21:48 +0430

    payment section of the charge_app created

--- charge_app/urls/resident_urls.py
+++ charge_app/urls/resident_urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from charge_app.views.resident_views import ChargeList
+from charge_app.views.resident_views import ChargeList, payment_charge,payment_charge_verify
 
 app_name = 'charge_app_resident'
 
 urlpatterns = [
     path('/charge-list', ChargeList.as_view(), name='charge_list'),
+    path('/payment-charge/<int:charge_status_pk>', payment_charge, name='payment_charge'),
+    path('/payment-charge-verify/<int:charge_status_pk>', payment_charge_verify, name='payment_charge_verify'),
 ]
--- charge_app/views/resident_views.py
+++ charge_app/views/resident_views.py
@@ -1,10 +1,15 @@
-from django.views.generic import ListView, CreateView,UpdateView,DetailView
+from django.http import Http404
+from django.utils import timezone
+from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, render, redirect
 
-from charge_app.models import ServiceCharge,ServiceChargeStatus
+from charge_app.models import ServiceCharge, ServiceChargeStatus
+from building_app.models import Building
 
-class ChargeList(ListView):
+
+class ChargeList(LoginRequiredMixin, ListView):
     model = ServiceChargeStatus
     template_name = 'resident/charge-list.html'
 
@@ -12,3 +17,37 @@ class ChargeList(ListView):
         unit = self.request.user.unit
         qs = ServiceChargeStatus.objects.filter(unit=unit)
         return qs
+
+
+def payment_charge(request, *args, **kwargs):  # send message function in zarinpal
+    unit = request.user.unit
+    charge_status_pk = kwargs.get('charge_status_pk')
+    service_charge_status = get_object_or_404(ServiceChargeStatus, id=charge_status_pk)
+    amount = int(service_charge_status.amount)
+
+    if service_charge_status.unit == unit and service_charge_status.is_paid == False:
+        # redirect to zarinpal - video 57 toplearn in local
+        # CallBackUrl = f'{payment-charge-verify}/{charge_status_pk}'
+        return redirect('/resident-panel/payment-charge-verify/' + str(charge_status_pk))
+
+    raise Http404()
+
+
+def payment_charge_verify(request, *args, **kwargs):
+    # if result.status == 100:
+    charge_status_pk = kwargs.get('charge_status_pk')
+    service_charge_status = get_object_or_404(ServiceChargeStatus, id=charge_status_pk)
+    amount = int(service_charge_status.amount)
+
+    # change status payment in ServiceChargeStatus model
+    service_charge_status.is_paid = True
+    service_charge_status.pay_time = timezone.now()
+    service_charge_status.save()
+
+    # change credit building
+    building = request.user.unit.building
+    building = get_object_or_404(Building, id=building.id)
+    building.credit += amount
+
+    context = {'status': 100}
+    return render(request, 'resident/payment-verify.html', context)
