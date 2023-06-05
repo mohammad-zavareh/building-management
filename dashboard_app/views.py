15e30e58bd1e0546aa38d90bf61a60673bc8e494
mohammad <mohammadzavareh78@gmail.com>
2023-06-06 00:51:31 +0430

    resident dashboard development

--- dashboard_app/views.py
+++ dashboard_app/views.py
@@ -1,10 +1,25 @@
 from django.views.generic import TemplateView
-
-from buildingManagement.mixins import ManagerRequiredMixin
+from django.db.models import Q
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class ManagerDashboard(LoginRequiredMixin, ManagerRequiredMixin,TemplateView):
+from buildingManagement.mixins import ManagerRequiredMixin
+from charge_app.models import ServiceChargeStatus
+from notification_app.models import Notification
+
+
+class ManagerDashboard(LoginRequiredMixin, ManagerRequiredMixin, TemplateView):
     template_name = 'manager_dashboard.html'
 
-class ResidentDashboard(LoginRequiredMixin,TemplateView):
+
+class ResidentDashboard(LoginRequiredMixin, TemplateView):
     template_name = 'resident_dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+
+        unit = self.request.user.unit
+        building = self.request.user.unit.building
+        data['unpaid_charges'] = ServiceChargeStatus.objects.filter(unit=unit, is_paid=False)[0:3]
+        data['unseen_notifications'] = Notification.objects.filter(Q(building_id=building),
+                                                                  ~Q(hits=unit)).order_by('-created')[0:3]
+        return data
