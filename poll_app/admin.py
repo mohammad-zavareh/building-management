09ab8174f870a5b650c6ac8567153f771b8ab2b1
mohammad <mohammadzavareh78@gmail.com>
2023-06-29 16:35:16 +0430

    completed poll_app admin

--- poll_app/admin.py
+++ poll_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Poll, PollOption
 
 # Register your models here.
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['building', '__str__', 'created']
+    list_filter = ['building']
+    ordering = ['-created']
+    search_fields = ['building', '__str__']
+
+
+class PollOptionAdmin(admin.ModelAdmin):
+    list_display = ['poll', '__str__']
+    list_filter = ['poll']
+    search_fields = ['poll', '__str__']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(PollOption, PollOptionAdmin)
